f13001d664cc4d1bde495cdf1b22f410c9b01b56
rkdtnlzl <rkdtnlzl@naver.com>
2024-04-03 00:30:41 +0900

    Feat [#7] TableViewCell 생성

---
+++ HOMEAT/Presentation/MyPageScene/Cell/MyPageTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  MyPageTableViewCell.swift
+//  HOMEAT
+//
+//  Created by 강석호 on 4/3/24.
+//
+
+import UIKit
+import SnapKit
+
+class MyPageTableViewCell: UITableViewCell {
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .bodyBold16
+        return label
+    }()
+    
+    let arrowButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "arrow.right"), for: .normal)
+        button.tintColor = .white
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        backgroundColor = UIColor(r: 30, g: 32, b: 33)
+        addSubviews(titleLabel,arrowButton)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(20)
+            make.centerY.equalToSuperview()
+        }
+        
+        arrowButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-20)
+            make.centerY.equalToSuperview()
+        }
+    }
+}
