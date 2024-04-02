fe3c138fa1587f16b5e6629b1d7472216756098c
befur1 <befur1@naver.com>
2024-04-03 01:02:43 +0900

    Feat [#5] HomeUI 라벨/버튼 구현
    - 공통 문서에서 StringLiterals 중 Home에 해당하는 부분을 수정했습니다
    - Label 구현
    - Button 구현
    - ButtonAction 구현
    - 캐릭터 표시하는 View 미구현
    - HomeView에 해당하는 Assets 추가

--- HOMEAT/Presentation/HomeScene/Controller/HomeViewController.swift
+++ HOMEAT/Presentation/HomeScene/Controller/HomeViewController.swift
@@ -11,15 +11,125 @@ import Then
 
 
 class HomeViewController: BaseViewController {
+    //MARK: - component
+    private let HomeatLogo = UIImageView()
+    private let welcomeLabel = UILabel()
+    private let savingLabel = UILabel()
+    private let payAddButton = UIButton()
+    private let payCheckButton = UIButton()
+    private let mainView = HomeView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
+        setAddTarget()
+        
     }
+    
     
     // MARK: - UI Setup
     private func setupUI() {
-        view.backgroundColor = .gray
+        view.backgroundColor = UIColor(named: "homeBackgroundColor")
+        
+        HomeatLogo.do {
+            $0.image = UIImage(named: "homeatTextLogo")
+        }
+        
+        welcomeLabel.do {
+            $0.text = StringLiterals.Home.main.welcome
+            $0.font = .headBold24
+            $0.textColor = .white
+        }
+        
+        savingLabel.do {
+            $0.text = StringLiterals.Home.main.saving
+            $0.font = .bodyBold18
+            $0.textColor = .white
+        }
+        
+        payAddButton.do {
+            $0.backgroundColor = UIColor(named: "turquoiseGreen")
+            $0.titleLabel?.font = .bodyBold18
+            $0.setImage(UIImage(named: "plusIcon"), for: .normal)
+            $0.setTitle("지출 추가", for: .normal)
+            $0.setTitleColor(.black, for: .normal)
+            $0.layer.cornerRadius = 10
+            $0.clipsToBounds = true
+            $0.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20)
+        }
+        
+        payCheckButton.do {
+            $0.backgroundColor = UIColor(named: "turquoiseGreen")
+            $0.titleLabel?.font = .bodyBold18
+            $0.setImage(UIImage(named: "checkIcon"), for: .normal)
+            $0.setTitle("지출 확인", for: .normal)
+            $0.setTitleColor(.black, for: .normal)
+            $0.layer.cornerRadius = 10
+            $0.clipsToBounds = true
+            $0.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20)
+        }
+
+
+
     }
+    
+    private func setAddTarget() {
+        payAddButton.addTarget(self, action: #selector(isPayAddButtonTapped), for: .touchUpInside)
+        
+        payCheckButton.addTarget(self, action: #selector(isPayCheckButtonTapped), for: .touchUpInside)
+    }
+    
+    override func setConstraints() {
+        view.addSubviews(HomeatLogo, welcomeLabel, savingLabel, payAddButton, payCheckButton, mainView)
+        
+        HomeatLogo.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(72)
+            $0.leading.equalToSuperview().offset(20)
+            
+        }
+        welcomeLabel.snp.makeConstraints{
+            $0.top.equalTo(HomeatLogo.snp.bottom).offset(27.7)
+            $0.leading.equalTo(HomeatLogo)
+        }
+        
+        savingLabel.snp.makeConstraints{
+            $0.top.equalTo(welcomeLabel.snp.bottom).offset(10)
+            $0.leading.equalTo(HomeatLogo)
+        }
+        
+        mainView.snp.makeConstraints{
+            $0.top.equalTo(savingLabel.snp.bottom).offset(20)
+            $0.leading.equalTo(HomeatLogo)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(353)
+        }
+        
+        payAddButton.snp.makeConstraints{
+            $0.top.equalTo(mainView.snp.bottom).offset(37)
+            $0.leading.equalTo(HomeatLogo)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(49)
+        }
+        
+        payCheckButton.snp.makeConstraints{
+            $0.top.equalTo(payAddButton.snp.bottom).offset(18)
+            $0.leading.equalTo(HomeatLogo)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(49)
+        }
+        
+    }
+    
+    //MARK: - @objc Func
+    
+    @objc func isPayAddButtonTapped(_ sender: Any) {
+        let nextVC = PayAddViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    @objc func isPayCheckButtonTapped(_ sender: Any) {
+        let nextVC = PayCheckViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+
 }
---
+++ HOMEAT/Presentation/HomeScene/Controller/PayAddViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PayAddViewController.swift
+//  HOMEAT
+//
+//  Created by 강삼고 on 4/2/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class PayAddViewController : BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    // MARK: - UI Setup
+    private func setupUI() {
+        view.backgroundColor = .blue
+    }
+}
