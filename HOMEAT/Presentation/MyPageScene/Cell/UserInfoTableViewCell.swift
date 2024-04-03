47cbdc70a3da0097155e6f9eaa7c2360006ed46d
rkdtnlzl <rkdtnlzl@naver.com>
2024-04-04 01:09:32 +0900

    feat [#19] UserInfoTableViewCell 생성

---
+++ HOMEAT/Presentation/MyPageScene/Cell/UserInfoTableViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  UserInfoTableViewCell.swift
+//  HOMEAT
+//
+//  Created by 강석호 on 4/3/24.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+class UserInfoTableViewCell: UITableViewCell {
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .bodyBold16
+        return label
+    }()
+    
+    let arrowButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+        button.tintColor = .white
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        backgroundColor = UIColor(r: 30, g: 32, b: 33)
+        addSubviews(titleLabel,arrowButton)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        
+        arrowButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-10)
+            make.centerY.equalToSuperview()
+        }
+    }
+}
