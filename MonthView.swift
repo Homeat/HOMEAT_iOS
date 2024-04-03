093a746f13253a5493213e478f25662e087e33aa
minsolkim <123alsthf@naver.com>
2024-04-04 02:02:13 +0900

    Fix [#18] 코드 수정사항 반영
    반영함

--- MonthView.swift
+++ MonthView.swift
@@ -31,18 +31,21 @@ final class MonthView: BaseView {
             $0.clipsToBounds = true
             $0.addTarget(self, action: #selector(monthBackTapped), for: .touchUpInside)
         }
+        
         monthNextButton.do {
             $0.setImage(UIImage(named: "calendarNext"), for: .normal)
             $0.contentMode = .scaleAspectFit
             $0.clipsToBounds = true
             $0.addTarget(self, action: #selector(monthNextTapped), for: .touchUpInside)
         }
+        
         yearMonthLabel.do {
             $0.text = "2024년 4월"
             $0.textColor = .white
             $0.textAlignment = .center
             $0.font = .bodyMedium18
         }
+        
         monthContentLabel.do {
             $0.text = "저번달 보다 8% 절약하고 있어요"
             $0.textColor = .white
@@ -59,33 +62,39 @@ final class MonthView: BaseView {
             $0.top.equalToSuperview().offset(19)
             $0.trailing.equalTo(yearMonthLabel.snp.leading).offset(-11)
         }
+        
         yearMonthLabel.snp.makeConstraints {
             $0.height.equalTo(26)
             $0.centerX.equalToSuperview()
             $0.top.equalToSuperview().offset(12)
         }
+        
         monthNextButton.snp.makeConstraints {
             $0.height.equalTo(14.6)
             $0.top.equalToSuperview().offset(19)
             $0.leading.equalTo(yearMonthLabel.snp.trailing).offset(11)
         }
+        
         monthContentLabel.snp.makeConstraints {
             $0.height.equalTo(30)
             $0.top.equalTo(yearMonthLabel.snp.bottom).offset(40)
             $0.centerX.equalToSuperview()
         }
+        
         pieChart.snp.makeConstraints {
             $0.top.equalTo(monthContentLabel.snp.bottom).offset(28)
             $0.leading.equalToSuperview().inset(30)
             $0.height.equalTo(145.5)
             $0.width.equalTo(147.6)
         }
+        
         barChartView.snp.makeConstraints {
             $0.top.equalTo(monthContentLabel.snp.bottom).offset(10)
             $0.leading.equalTo(pieChart.snp.trailing).offset(20)
             $0.trailing.equalToSuperview().inset(45.7)
             $0.bottom.equalToSuperview().inset(30)
         }
+        
     }
     
     override func setting() {
@@ -98,11 +107,11 @@ final class MonthView: BaseView {
         entries.append(PieChartDataEntry(value: Double(100-remainingPercent)))
         let dataSet = PieChartDataSet(entries: entries)
         if let customGreenColor = UIColor(named: "turquoiseGreen"),
-            let otherColor = UIColor(named: "turquoisePurple") {
-                let nsCustomGreenColor = NSUIColor(cgColor: customGreenColor.cgColor)
-                let nsWhiteColor = NSUIColor.black
-                let nsOtherColor = NSUIColor(cgColor: otherColor.cgColor)
-                dataSet.colors = [nsCustomGreenColor, nsOtherColor]
+           let otherColor = UIColor(named: "turquoisePurple") {
+            let nsCustomGreenColor = NSUIColor(cgColor: customGreenColor.cgColor)
+            let nsWhiteColor = NSUIColor.black
+            let nsOtherColor = NSUIColor(cgColor: otherColor.cgColor)
+            dataSet.colors = [nsCustomGreenColor, nsOtherColor]
         }
         //piechart edge 삭제
         dataSet.selectionShift = 0
