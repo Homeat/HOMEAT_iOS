6da847479f463d63b2276f851616640c7144fdd2
KangSangJun <befur1@naver.com>
2024-04-16 02:15:01 +0900

    feat [#22] foodTalkUI 부분 다시 추가

--- HOMEAT/Presentation/TalkScene/Controller/TalkViewController.swift
+++ HOMEAT/Presentation/TalkScene/Controller/TalkViewController.swift
@@ -6,17 +6,87 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+import Tabman
+import Pageboy
 
-class TalkViewController: BaseViewController {
+class TalkViewController: TabmanViewController {
     
+    private var viewcontrollers : Array<UIViewController> = [] //뷰 컨트롤러의 뷰를 넣을 배열
+    private let containerView = UIView()
+    private let tabBar = TMBar.ButtonBar()
+    
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        super.viewDidLoad()
+        self.title = "홈잇토크"
+        setViewControllers()
+        setConstraints()
+        setConfigure()
         setupUI()
     }
     
     // MARK: - UI Setup
     private func setupUI() {
-        view.backgroundColor = .green
+        self.navigationController?.navigationBar.titleTextAttributes =  [NSAttributedString.Key.foregroundColor: UIColor.white]
+        view.backgroundColor = UIColor(r: 30, g: 32, b: 33)
+    }
+    
+    private func setConfigure() {
+        tabBar.do {
+            $0.backgroundView.style = .clear
+            $0.layout.transitionStyle = .snap
+            $0.layout.contentMode = .fit
+            $0.buttons.customize { button in
+                button.selectedTintColor = .white
+                button.tintColor = .gray
+            }
+            $0.indicator.weight = .custom(value: 2)
+            $0.indicator.tintColor = .white
+        }
+        containerView.do {
+            $0.backgroundColor = .clear
+        }
+    }
+    
+    private func setViewControllers() {
+        let foodTalkViewController = FoodTalkViewController()
+        let weeklookViewController = WeekLookViewController()
+        
+        viewcontrollers.append(contentsOf: [foodTalkViewController, weeklookViewController])
+    }
+    
+    private func setConstraints() {
+        tabBar.dataSource = self
+        dataSource = self
+        addBar(tabBar, dataSource: self, at: .custom(view: containerView, layout: nil))
+        view.addSubview(containerView)
+        containerView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(40)
+        }
+    }
+}
+
+// MARK: - Extension
+extension TalkViewController: PageboyViewControllerDataSource, TMBarDataSource {
+    
+    func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
+        return viewcontrollers.count
+    }
+    
+    func viewController(for pageboyViewController: PageboyViewController, at index: PageboyViewController.PageIndex) -> UIViewController? {
+        return viewcontrollers[index]
+    }
+    
+    func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
+        return nil
+    }
+    
+    func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
+        let title = index == 0 ? "집밥토크" : "정보토크"
+        return TMBarItem(title: title)
     }
 }
