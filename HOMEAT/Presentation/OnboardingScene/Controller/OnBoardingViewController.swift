b387901e54cfe96edca80643b439cc35ca99a84a
rkdtnlzl <rkdtnlzl@naver.com>
2024-04-17 20:48:20 +0900

    Feat [#38] 온보딩 VC UI 작업

---
+++ HOMEAT/Presentation/OnboardingScene/Controller/OnBoardingViewController.swift
@@ -0,0 +1,87 @@
+//
+//  LoginViewController.swift
+//  HOMEAT
+//
+//  Created by 강석호 on 4/17/24.
+//
+
+import Foundation
+import UIKit
+import Then
+import SnapKit
+
+class OnBoardingViewController : BaseViewController {
+    
+    private let introTitleLabel = UILabel()
+    private let welcomeView = WelcomeView()
+    private let signupButton = UIButton()
+    private let signinButton = UIButton()
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(r: 30, g: 32, b: 33)
+    }
+    
+    override func setConfigure() {
+        introTitleLabel.do {
+            $0.text = "1인 가구를 위한 식비 첼린지 커뮤니티"
+            $0.font = .bodyMedium18
+            $0.textColor = .turquoiseGreen
+        }
+        
+        signupButton.do {
+            $0.backgroundColor = UIColor(named: "turquoiseGreen")
+            $0.titleLabel?.font = .bodyMedium18
+            $0.setTitle("가입하기", for: .normal)
+            $0.setTitleColor(.black, for: .normal)
+            $0.layer.cornerRadius = 10
+            $0.clipsToBounds = true
+        }
+        
+        signinButton.do {
+            $0.backgroundColor = UIColor(r: 30, g: 32, b: 33)
+            $0.titleLabel?.font = .bodyMedium18
+            $0.setTitle("계정이 이미 있어요", for: .normal)
+            $0.setTitleColor(.turquoiseGreen, for: .normal)
+            $0.layer.cornerRadius = 10
+            $0.clipsToBounds = true
+            $0.layer.borderColor = UIColor.turquoiseGreen.cgColor
+            $0.layer.borderWidth = 1
+        }
+        
+    }
+    
+    override func setConstraints() {
+        view.addSubviews(introTitleLabel, welcomeView, signupButton, signinButton)
+        
+        introTitleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(280)
+        }
+        
+        welcomeView.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.top.equalTo(introTitleLabel.snp.bottom).offset(15)
+        }
+        
+        signupButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.bottom.equalTo(signinButton.snp.top).offset(-25)
+            $0.height.equalTo(57)
+        }
+        
+        signinButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.bottom.equalToSuperview().offset(-76)
+            $0.height.equalTo(57)
+        }
+
+        
+        
+    }
+    
+}
