b35b340cb83410ec7a8099242637347b14b39c34
rkdtnlzl <rkdtnlzl@naver.com>
2024-04-29 16:31:16 +0900

    [27] Feat [#48] Response 베이스코드 추가

---
+++ HOMEAT/Network/Base/BaseResponse.swift
@@ -0,0 +1,14 @@
+//
+//  BaseResponse.swift
+//  HOMEAT
+//
+//  Created by 강석호 on 4/29/24.
+//
+
+import Foundation
+
+struct BaseResponse<T: Decodable>: Decodable {
+    let code: String
+    let message: String
+    let data: T?
+}
