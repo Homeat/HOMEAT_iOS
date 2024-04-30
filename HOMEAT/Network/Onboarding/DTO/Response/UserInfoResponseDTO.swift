0eb8a9b0b7f223bc45acc064c22014e966bedfd7
rkdtnlzl <rkdtnlzl@naver.com>
2024-04-30 14:59:46 +0900

    [27] Feat [#48] 유저정보 dto 추가

---
+++ HOMEAT/Network/Onboarding/DTO/Response/UserInfoResponseDTO.swift
@@ -0,0 +1,14 @@
+//
+//  UserInfoResponseDTO.swift
+//  HOMEAT
+//
+//  Created by 강석호 on 4/29/24.
+//
+
+import Foundation
+
+struct UserInfoResponseDTO: Codable {
+    let id: Int
+    let name: String?
+}
+
