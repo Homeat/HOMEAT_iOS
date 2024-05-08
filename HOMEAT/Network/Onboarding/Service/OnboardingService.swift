f183bb8201352262697431700d607d00679aa74b
rkdtnlzl <rkdtnlzl@naver.com>
2024-05-08 20:58:17 +0900

    [HOMEAT-36] Feat [#60] 이메일 로그인 DTO 및 서비스 추가

--- HOMEAT/Network/Onboarding/Service/OnboardingService.swift
+++ HOMEAT/Network/Onboarding/Service/OnboardingService.swift
@@ -8,15 +8,16 @@
 import Foundation
 
 protocol OnboardingServiceProtocol {
-    func login(bodyDTO: KakaoLoginRequestBodyDTO, completion: @escaping (NetworkResult<BaseResponse<KakaoLoginResponseDTO>>) -> Void)
+    func kakaoLogin(bodyDTO: KakaoLoginRequestBodyDTO, completion: @escaping (NetworkResult<BaseResponse<KakaoLoginResponseDTO>>) -> Void)
     func userInfo(completion: @escaping (NetworkResult<BaseResponse<UserInfoResponseDTO>>) -> Void)
     func postRefreshToken(completion: @escaping (NetworkResult<BaseResponse<TokenRefreshResponseDTO>>) -> Void)
+    func emailLogin(bodyDTO: EmailLoginRequestBodyDTO, completion: @escaping (NetworkResult<BaseResponse<EmailLoginResponseDTO>>) -> Void)
 }
 
 final class OnboardingService: APIRequestLoader<OnboardingTarget>, OnboardingServiceProtocol {
     
-    func login(bodyDTO: KakaoLoginRequestBodyDTO, completion: @escaping (NetworkResult<BaseResponse<KakaoLoginResponseDTO>>) -> Void) {
-        fetchData(target: .login(bodyDTO),
+    func kakaoLogin(bodyDTO: KakaoLoginRequestBodyDTO, completion: @escaping (NetworkResult<BaseResponse<KakaoLoginResponseDTO>>) -> Void) {
+        fetchData(target: .kakaoLogin(bodyDTO),
                   responseData: BaseResponse<KakaoLoginResponseDTO>.self, completion: completion)
     }
     
@@ -27,5 +28,10 @@ final class OnboardingService: APIRequestLoader<OnboardingTarget>, OnboardingSer
     
     func postRefreshToken(completion: @escaping (NetworkResult<BaseResponse<TokenRefreshResponseDTO>>) -> Void) {
         fetchData(target: .postRefreshToken, responseData: BaseResponse<TokenRefreshResponseDTO>.self, completion: completion)
+    }
+    
+    func emailLogin(bodyDTO: EmailLoginRequestBodyDTO, completion: @escaping (NetworkResult<BaseResponse<EmailLoginResponseDTO>>) -> Void) {
+        fetchData(target: .emailLogin(bodyDTO),
+                  responseData: BaseResponse<EmailLoginResponseDTO>.self, completion: completion)
     }
 }
