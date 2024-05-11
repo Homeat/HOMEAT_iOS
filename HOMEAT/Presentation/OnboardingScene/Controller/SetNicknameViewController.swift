1e9a1f4a5a2a22db2e8563ff38d9909207c91b14
KangSangJun <befur1@naver.com>
2024-05-11 16:43:35 +0900

    [HOMEAT-39] Feat [#62] 닉네임 생성 페이지 구현

---
+++ HOMEAT/Presentation/OnboardingScene/Controller/SetNicknameViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SetNicknameViewController.swift
+//  HOMEAT
+//
+//  Created by 강삼고 on 5/8/24.
+//
+
+import Foundation
+import UIKit
+import Then
+import SnapKit
+
+class SetNicknameViewController: ProgressViewController {
+    
+    private let nameLabel = UILabel()
+    private let nameTextField = UITextField()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateProgressBar(progress: 1/5)
+        setTitleLabel(title: "생년월일을\n입력해주세요.")
+        setSubTitleLabel(subtitle: "닉네임")
+        setTextField(textField: nameTextField)
+        setNextVC(nextVC: SetBirthViewController())
+    }
+    
+    override func setConfigure() {
+        super.setConfigure()
+    
+        nameTextField.do {
+            $0.backgroundColor = UIColor(r: 54, g: 56, b: 57)
+            $0.makeCornerRound(radius: 10)
+            let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 18, height: $0.frame.height))
+            $0.leftView = leftPaddingView
+            $0.leftViewMode = .always
+            $0.delegate = self
+        }
+
+    }
+    
+    override func setConstraints() {
+        super.setConstraints()
+        view.addSubviews(nameTextField)
+        
+        nameTextField.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(369)
+            $0.leading.equalToSuperview().offset(20)
+            $0.height.equalTo(57)
+            $0.trailing.equalToSuperview().offset(-20)
+        }
+    }
+}
