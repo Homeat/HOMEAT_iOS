afde1ddeb1b7d4d600c9361e89e5df55a297118d
minsolkim <123alsthf@naver.com>
2024-06-24 15:35:38 +0900

    Feat[#66] 주별조회 target및 service 추가

--- HOMEAT/Network/WeekLook/Router/WeekLookTarget.swift
+++ HOMEAT/Network/WeekLook/Router/WeekLookTarget.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import Alamofire
+
+
+enum WeekLookTarget {
+    case weekLook(_ queryDTO : WeekLookRequestBodyDTO)
+}
+
+extension WeekLookTarget: TargetType {
+    var authorization: Authorization {
+        switch self {
+        case .weekLook:
+            return .authorization
+        }
+    }
+    
+    var headerType: HTTPHeaderType {
+        switch self {
+        case .weekLook:
+            return .hasToken
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .weekLook:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .weekLook:
+            return "/v1/badgeReport/Badge"
+        }
+    }
+    
+    var parameters: RequestParams {
+        switch self {
+        case .weekLook(let queryDTO):
+            return .requestQuery(queryDTO)
+        }
+    }
+
+}
