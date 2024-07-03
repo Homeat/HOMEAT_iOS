1ac99b3a7ecababbfd5011dc598d81b96791a384
minsolkim <123alsthf@naver.com>
2024-07-04 00:57:56 +0900

    Feat[#78] service

--- HOMEAT/Network/WeekLook/Service/WeekLookService.swift
+++ HOMEAT/Network/WeekLook/Service/WeekLookService.swift
@@ -11,9 +11,9 @@ protocol WeekLookServiceProtocol {
     func weekLookReport(queryDTO: WeekLookRequestBodyDTO, completion: @escaping (NetworkResult<BaseResponse<WeekLookResponseDTO>>) -> Void)
 }
 
+// 서비스 클래스 정의
 final class WeekLookService: APIRequestLoader<WeekLookTarget>, WeekLookServiceProtocol {
     func weekLookReport(queryDTO: WeekLookRequestBodyDTO, completion: @escaping (NetworkResult<BaseResponse<WeekLookResponseDTO>>) -> Void) {
         fetchData(target: .weekLook(queryDTO), responseData: BaseResponse<WeekLookResponseDTO>.self, completion: completion)
     }
-    
 }
